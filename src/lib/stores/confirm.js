376995450f3779ec9902a286727172690b3cf6eb
Joshua Ravael <jravaellnew@gmail.com>
2026-01-29 10:56:04 +0700

    add confirmation dialog store

---
+++ src/lib/stores/confirm.js
@@ -0,0 +1,34 @@
+import { writable } from 'svelte/store';
+
+const initial = {
+    active: false,
+    title: '',
+    message: '',
+    confirmText: 'Confirm',
+    cancelText: 'Cancel',
+    dangerous: false,
+    resolve: null
+};
+
+export const confirmState = writable({ ...initial });
+
+export const askConfirm = ({ title, message, confirmText = 'Confirm', cancelText = 'Cancel', dangerous = false }) => {
+    return new Promise((resolve) => {
+        confirmState.set({
+            active: true,
+            title,
+            message,
+            confirmText,
+            cancelText,
+            dangerous,
+            resolve
+        });
+    });
+};
+
+export const closeConfirm = (result) => {
+    confirmState.update(s => {
+        if (s.resolve) s.resolve(result);
+        return { ...s, active: false };
+    });
+};
