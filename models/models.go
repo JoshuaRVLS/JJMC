457703684cb0edf18495ccd40a5d76159de883f6
Joshua Ravael <jravaellnew@gmail.com>
2026-01-30 09:11:58 +0700

    feat: Extract models to shared package

---
+++ models/models.go
@@ -0,0 +1,24 @@
+package models
+
+type Instance struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Directory string `json:"directory"`
+	Type      string `json:"type"` // e.g. "fabric", "vanilla"
+	Version   string `json:"version"`
+	Status    string `json:"status"` // "Online" or "Offline"
+	MaxMemory int    `json:"maxMemory"`
+	JavaArgs  string `json:"javaArgs"`
+	JarFile   string `json:"jarFile"`
+}
+
+type InstanceModel struct {
+	ID        string `gorm:"primaryKey"`
+	Name      string
+	Type      string
+	Version   string
+	MaxMemory int
+	JavaArgs  string
+	JarFile   string
+	CreatedAt int64
+}
